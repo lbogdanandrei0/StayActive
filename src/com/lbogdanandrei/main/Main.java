c731f1205d9ab9f59a48646315468f902bbbdead
lbogdanandrei0 <lungubogdanandrei0@gmail.com>
2021-01-17 20:09:05 +0200

    Added ctrl+y as a stop method

--- src/com/lbogdanandrei/main/Main.java
+++ src/com/lbogdanandrei/main/Main.java
@@ -4,8 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 class MainWindow extends JFrame implements ActionListener, ItemListener
@@ -71,6 +73,17 @@ class MainWindow extends JFrame implements ActionListener, ItemListener
                 coords.setText(newWidth + " & " + newHeight);
             }
         });
+        
+        start.getInputMap().put(KeyStroke.getKeyStroke("control Y"), "key stop");
+        start.getActionMap().put("key stop", new AbstractAction()
+        		{
+					private static final long serialVersionUID = 1L;
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						stop.doClick();
+					}
+        		});
     }
 
     @Override
